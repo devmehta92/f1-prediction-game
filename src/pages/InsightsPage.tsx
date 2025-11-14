143760c573a252b21399a8778643072e20a47952
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:22:19 -0800

    Add InsightsPage page

---
+++ src/pages/InsightsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InsightsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Insights</h1>
+      <p>Explore insights and analytics.</p>
+    </div>
+  );
+};
+
+export default InsightsPage;
