6f2de9e4595b383d51951667d8cf5b925e169126
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:16:31 -0800

    Add RegisterPage page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>Create an account to start playing.</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
