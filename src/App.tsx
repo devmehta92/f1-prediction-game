a0dabc769a0408e245d4f535783219d3a9746ff1
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:00:19 -0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>F1 Prediction Game</h1>
+      <p>Welcome to the F1 Prediction Game! This app is under development.</p>
+    </div>
+  );
+};
+
+export default App;
