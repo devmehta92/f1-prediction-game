58640d7552337833a440de11d5e45aab12490c36
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:18:09 -0800

    Add TeamPage page

---
+++ src/pages/TeamPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TeamPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Team Builder</h1>
+      <p>Build your fantasy team here.</p>
+    </div>
+  );
+};
+
+export default TeamPage;
