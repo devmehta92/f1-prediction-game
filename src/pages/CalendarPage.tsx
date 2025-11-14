0331b4392993b3b042533ae6f570e1ac3bf05656
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:19:05 -0800

    Add CalendarPage page

---
+++ src/pages/CalendarPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendarPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Race Calendar</h1>
+      <p>Explore the race calendar for the season.</p>
+    </div>
+  );
+};
+
+export default CalendarPage;
