913494fa7ecb2c3acdea5f1ac3b2b490c23f73d8
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:23:10 -0800

    Add AdminPage page

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <p>Manage drivers, constructors, races, and scoring rules here.</p>
+    </div>
+  );
+};
+
+export default AdminPage;
