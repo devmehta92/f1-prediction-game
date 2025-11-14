801ba42870162ab4d3dffab6660412217eb053a8
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:20:49 -0800

    Add LeaguesPage page

---
+++ src/pages/LeaguesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LeaguesPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Leagues</h1>
+      <p>Manage and explore your leagues.</p>
+    </div>
+  );
+};
+
+export default LeaguesPage;
