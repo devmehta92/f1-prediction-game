87b2807be7643fb94f9ea14613a8aaf7e0dfea58
Dev Mehta <devmehta0492@gmail.com>
2025-11-13 19:15:10 -0800

    Add HomePage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>F1 Fantasy Game</h1>
+      <p>Welcome to the F1 Fantasy Game. Build your team for the season!</p>
+    </div>
+  );
+};
+
+export default HomePage;
